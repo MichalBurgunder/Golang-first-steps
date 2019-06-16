d2bb23c03f5b622a6f443d1853367975b73592e1
Michal <michal_burgunder@yahoo.com>
2019-06-16 08:17:38 +0200

    beginning to learn pointers

---
+++ src/playground.go
@@ -0,0 +1,31 @@
+package playground
+
+import (
+	"fmt"
+)
+
+func LetsPlay(str, secondstr, thirdstr string) string {
+	const t = "Hello there"
+	// err := errors.New("Something went wrong")
+	return str
+}
+
+// func somePointer(*p) string {
+// 	return "hello there"
+// }
+
+func playgroundFunction(str string) string {
+	var x string
+	x = "Hello there" + x
+	return x
+}
+
+func main(int, []string) int {
+	var a []int
+	// var x = a[1]
+	a = append(a, 66)
+	// x = append(x, [66]int)
+
+	fmt.Printf("Ahhhh, general Kenobi! What a pleasant surprise")
+	return 10
+}
